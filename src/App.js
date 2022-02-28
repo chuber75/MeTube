77cd73e3bc8fb60e229d6b6d5c507a19f51e854e
gage <grindt@clemson.edu>
2022-02-28 15:06:45 -0500

    merge

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import TempBody from "./components/TempBody";
+
+function App() {
+  return (
+      <div>
+          <Navbar />
+          <TempBody />
+      </div>
+  );
+}
+
+export default App;
